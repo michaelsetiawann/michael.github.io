1cfa104410f9590b0a0835cb817794540ace6ed0
glennprays <glennsagala@gmail.com>
2023-08-15 18:40:15 +0700

    disaable text select

--- src/components/header/Navigation.tsx
+++ src/components/header/Navigation.tsx
@@ -2,9 +2,9 @@ import { ThemeSwicher } from "@/components/theme/ThemeSwicher";
 import Link from "next/link";
 
 export const Navigation = () => {
-    return (
-        <nav className="py-5 px-8 flex items-center justify-between z-50 bg-inherit">
-            <Link id="logo" href='/' className="font-mono font-bold text-2xl">
+    return ( 
+        <nav className="py-5 px-8 flex items-center justify-between z-50 bg-inherit select-none">
+            <Link id="logo" href='/' className="font-mono font-bold text-2xl select-none">
                 glennprays;
             </Link>
             <ThemeSwicher />
