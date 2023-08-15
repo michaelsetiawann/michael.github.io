6c98c0ddf786b312b382a239ed46d9ac24869cdd
glennprays <glennsagala@gmail.com>
2023-08-15 15:35:10 +0700

    feature: theme switcher

--- src/components/theme/ThemeSwicher.tsx
+++ src/components/theme/ThemeSwicher.tsx
@@ -2,30 +2,36 @@
 
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
 
 export function ThemeSwicher() {
     const [mounted, setMounted] = useState(false);
-    const {setTheme, resolvedTheme } = useTheme();
+    const [isOn, setIsOn] = useState(false);
+    const { setTheme, resolvedTheme } = useTheme();
 
     useEffect(() => setMounted(true), []);
 
     if (!mounted) return null;
     const isDark = resolvedTheme === 'dark';
 
+    const toggleSwitch = () => {setIsOn(isDark); setTheme(isDark ? 'light' : 'dark')};
+
     return (
-        <div className="flex flex-col items-center">
-                <button
-                    className="rounded-md border-purple-400 border-2 p-4"
-                    onClick={() => setTheme(isDark ? 'light' : 'dark')}
-                >
-                    <span className="text-xl">
-                        {
-                            isDark ? <MdOutlineDarkMode /> : <MdOutlineLightMode />
-                        }
-                    </span>
-                </button>
-                
+        <div className={('w-14 border-black bg-slate-400 rounded-full flex items-center cursor-pointer ' + (isOn ? 'justify-end' : 'justify-start'))} onClick={toggleSwitch}>
+            <motion.div
+                className="bg-gradient-to-r from-amber-300 via-amber-200 to-amber-100 dark:from-indigo-600 dark:via-purple-600 dark:to-pink-600  w-7 h-7 rounded-full shadow-md flex items-center justify-center"
+                layout
+                transition={spring}
+            >
+                {isDark ? <MdOutlineDarkMode className="text-white" /> : <MdOutlineLightMode className="text-black" />}
+            </motion.div>
         </div>
     )
 }
+
+const spring = {
+    type: "spring",
+    stiffness: 500,
+    damping: 30
+};
