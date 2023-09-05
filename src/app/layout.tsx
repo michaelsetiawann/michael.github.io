957169d39ef02e1daf3fc14180ba2e6a461575de
glennprays <glennsagala@gmail.com>
2023-09-05 12:47:40 +0700

    refactor layout stucture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,54 @@
-import Provider from '@/components/middleware/provider'
-import './globals.css'
-import { Poppins } from 'next/font/google'
-import { Navigation } from '@/components/header/Navigation'
-import Footer from '@/components/footer/Footer';
+import Provider from "@/components/middleware/provider";
+import "./globals.css";
+import { Poppins } from "next/font/google";
+import Footer from "@/components/footer/Footer";
+import { hostName } from "@/constans/general";
 
-const fontFamily = Poppins({ weight: ['100', '200', '400', '500', '600', '700', '800', '900', '300'], subsets: ['latin'] });
+const fontFamily = Poppins({
+    weight: ["100", "200", "400", "500", "600", "700", "800", "900", "300"],
+    subsets: ["latin"],
+});
 
 export const metadata = {
-  metadataBase: new URL('https://glennprays.tech'),
-  title: {
-    default: 'Homepage',
-    template: `%s | glennprays;`,
-    keywords: ['Glenn Pray Sabbatico Matthew', 'Glenn Pray Website', 'Glenn Pray Sagala', 'glennprays']
-  },
-  description: 'Hello, i\'m Glenn Pray. Here, you will find a comprehensive collection of my professional journey, including my resume, portfolio, showcased projects, and blog posts.',
-  viewport: {
-    width: 'device-width',
-    height: 'device-height',
-    initialScale: 1,
-    maximumScale: 1,
-    minimumScale: 1,
-  },
+    metadataBase: new URL(hostName),
+    title: {
+        default: "Homepage",
+        template: `%s | glennprays;`,
+        keywords: [
+            "Glenn Pray Sabbatico Matthew",
+            "Glenn Pray Website",
+            "Glenn Pray Sagala",
+            "glennprays",
+        ],
+    },
+    description:
+        "Hello, i'm Glenn Pray. Here, you will find a comprehensive collection of my professional journey, including my resume, portfolio, showcased projects, and blog posts.",
+    viewport: {
+        width: "device-width",
+        height: "device-height",
+        initialScale: 1,
+        maximumScale: 1,
+        minimumScale: 1,
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={fontFamily.className}>
-        <Provider>
-          <div className='min-h-screen w-full overflow-x-hidden flex flex-col bg-neutral-200 dark:bg-neutral-900 pt-24'>
-            <Navigation />
-            <div className='px-8 flex-grow'>
-              {children}
-            </div>
-            <Footer />
-          </div>
-        </Provider>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={fontFamily.className}>
+                <Provider>
+                    <div className="bg-neutral-200 dark:bg-neutral-900">
+                        <div className="min-h-screen w-full overflow-x-hidden flex flex-col pt-24">
+                            {children}
+                        </div>
+                        <Footer />
+                    </div>
+                </Provider>
+            </body>
+        </html>
+    );
 }
