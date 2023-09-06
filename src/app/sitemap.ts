c54e33445daeebb2a1415d830c97b05f3fd7f98f
glennprays <glennsagala@gmail.com>
2023-09-06 15:49:23 +0700

    feature: unfinish blog og image

---
+++ src/app/sitemap.ts
@@ -0,0 +1,20 @@
+import { hostName } from "@/constans/general";
+import { getAllBlogPost } from "./(blogs)/blog/_utils/getData";
+
+export default async function sitemap() {
+    const baseUrl = hostName;
+
+    const blogs = getAllBlogPost();
+
+    const blogsUrls = blogs.map((blog) => ({
+        url: `${baseUrl}/blog/${blog.slug}`,
+        lastModified: new Date(blog.meta.date),
+    }));
+
+    return [
+        { url: baseUrl, lastModified: new Date() },
+        { url: `${baseUrl}/blog`, lastModified: new Date() },
+
+        ...blogsUrls,
+    ];
+}
